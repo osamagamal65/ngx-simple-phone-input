6fe7d101240b110e6dede38dba5b9d61d6073bf5
Osama Gamal <ogamalii65@gmail.com>
2023-01-25 11:49:35 +0200

    update package public api

--- projects/ngx-simple-phone-input/src/public-api.ts
+++ projects/ngx-simple-phone-input/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-simple-phone-input
  */
 
-export * from './lib/ngx-simple-phone-input.service';
-export * from './lib/ngx-simple-phone-input.component';
-export * from './lib/ngx-simple-phone-input.module';
+export * from './lib/ngx-phone-input.service';
+export * from './lib/ngx-phone-input.component';
+export * from './lib/ngx-phone-input.module';
